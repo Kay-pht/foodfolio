611f3b71014691c6d4e1e020691ca6cabe45b255
Kay Yasuda <182742906+Kay-pht@users.noreply.github.com>
2026-09-07 13:33:28 +0900

    style: keep Terraform diff focused

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -137,6 +137,7 @@ resource "google_project_iam_member" "github_roles" {
   role    = each.value
   member  = "serviceAccount:${google_service_account.github_deployer.email}"
 }
+
 resource "google_service_account_iam_member" "github_act_as_api" {
   service_account_id = google_service_account.api.name
   role               = "roles/iam.serviceAccountUser"
