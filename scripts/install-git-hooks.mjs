af9fe41c539c50d44b62a4a0fd11050e445f76cc
Kay Yasuda <182742906+Kay-pht@users.noreply.github.com>
2026-09-09 17:16:35 +0900

    feat: add git hook installer

---
+++ scripts/install-git-hooks.mjs
@@ -0,0 +1,4 @@
+import { execFileSync } from "node:child_process";
+
+execFileSync("git", ["config", "core.hooksPath", ".githooks"], { stdio: "inherit" });
+console.log("Configured git hooks: core.hooksPath=.githooks");
